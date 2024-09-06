ed153cfd2ceb2e0c67637a0b238b72fcda5e6dcb
Emmanuel Osamau <151487073+EOsamau@users.noreply.github.com>
2024-09-06 09:30:10 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,29 @@
+# %%
+import statsmodels.formula.api as smf
+import pandas as pd
+
+data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
+
+# %%
+data.head()
+
+# %%
+model = smf.ols("trips ~ hour", data=data)
+
+modelFit = model.fit()
+
+modelFit.summary()
+
+# %%
+test_data = pd.read_csv('https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv')
+test_data = test_data[['hour']]
+test_data.head(10)
+
+# %%
+pred = modelFit.predict(test_data)
+
+
+
+
+
 
